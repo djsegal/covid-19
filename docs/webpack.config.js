36250f27c213ba0ca6b68ace4de74729ac069acc
Dan Segal <dansegal2@gmail.com>
2020-04-06 22:46:24 -0700

    Fix bug in webpack config where /dist would not load in prod

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -38,7 +38,7 @@ module.exports = {
         }),
         new ManifestPlugin({
             fileName: '../_data/manifest.yml',
-            publicPath: './dist/',
+            publicPath: isProduction ? './covid-19/dist/' : './dist/',
         }),
     ],
 };
