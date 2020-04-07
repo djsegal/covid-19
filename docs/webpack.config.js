24cb80376a466ce9c14b2be499c6140ad0999f6c
Dan Segal <dansegal2@gmail.com>
2020-04-07 03:33:37 -0700

    Move index.css inside assets/css subdirectory

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -7,7 +7,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
     mode: isProduction ? 'production' : 'development',
     entry: {
-        docs: path.resolve(__dirname, './assets/index.css'),
+        docs: path.resolve(__dirname, './assets/css/index.css'),
     },
     output: {
         path: path.resolve(__dirname, './dist/'),
