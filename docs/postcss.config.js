ea3e0ed0ba5d47c191923700a8bf053804b95972
Dan Segal <dansegal2@gmail.com>
2020-04-06 23:29:57 -0700

    Add tailwind ui to project

--- docs/postcss.config.js
+++ docs/postcss.config.js
@@ -3,12 +3,12 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
         // Jekyll output directory
         './_site/**/*.html',
     ],
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+    defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || []
 });
 
 module.exports = {
     plugins: [
-        require('tailwindcss'),
+        require("tailwindcss")("./tailwind.config.js"),
         require('cssnano')(),
         require('autoprefixer'),
         ...process.env.NODE_ENV === 'production'
