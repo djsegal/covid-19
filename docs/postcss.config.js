0de43797d89d81baa70cd318bc750d1b7b2bfc2f
Dan Segal <dansegal2@gmail.com>
2020-04-29 15:06:30 -0700

    Add whitelisted css list to purgecss boot loader

--- docs/postcss.config.js
+++ docs/postcss.config.js
@@ -1,9 +1,21 @@
+const curWhitelist = [
+    "noUi-connect"
+]
+
+const curWhitelistPatterns = [
+    /bg-$/, /text-$/, /border-$/,
+    /p-$/, /pl-$/, /pr-$/, /pt-$/, /pb-$/, /py-$/, /px-$/,
+    /m-$/, /ml-$/, /mr-$/, /mt-$/, /mb-$/, /my-$/, /mx-$/
+]
+
 const purgecss = require('@fullhuman/postcss-purgecss')({
     content: [
         // Jekyll output directory
         './_site/**/*.html',
     ],
-    defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || []
+    defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || [],
+    whitelist: curWhitelist,
+    whitelistPatterns: curWhitelistPatterns
 });
 
 module.exports = {
